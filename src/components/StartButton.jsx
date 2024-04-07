f747751dcac22a095a5916b36a243f43dbd84452
hortfrancis <ajhf@hotmail.co.uk>
2024-04-07 17:38:38 +0100

    Add interactivity styling for Start button

--- src/components/StartButton.jsx
+++ src/components/StartButton.jsx
@@ -1,11 +1,18 @@
 export default function StartButton() {
     return (
-        <button className={`block bg-white text-black 
-        shadow-[0px_14px_0px_6px_black,0px_0px_0px_6px_black] 
-        rounded-xl w-80 h-80`}>
-        <span className="text-6xl font-extrabold">
-            Start
-        </span>
+        <button className={`
+            block bg-white text-black 
+            shadow-[0px_14px_0px_6px_black] 
+            ring-[6px] ring-black
+            rounded-xl w-80 h-80
+            hover:bg-slate-100
+            active:shadow-[0px_0px_0px_6px_black]
+            active:translate-y-[14px]
+            transition-all duration-100 ease-in-out
+        `}>
+            <span className="text-6xl font-extrabold">
+                Start
+            </span>
         </button>
     )
 }
