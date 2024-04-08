7ab2a7edfcc682a433830bb287758f6b32d3eb66
hortfrancis <ajhf@hotmail.co.uk>
2024-04-08 21:56:54 +0100

    Add text output with typing effect
    - Static assistant audio is accompanied by text, with types out one letter at a time

--- src/components/AssistantTextOutputArea.jsx
+++ src/components/AssistantTextOutputArea.jsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from 'react';
 import { useAssistant } from "../contexts"
 
 export default function AssistantTextOutputArea() {
-
     const { assistantTextOutput } = useAssistant();
+    const [displayedText, setDisplayedText] = useState('');
+
+    useEffect(() => {
+        setDisplayedText(''); 
+        
+        if (assistantTextOutput.length === 0) {
+            return; // If there's no text, don't run the typing effect.
+        }
+
+        const timer = setInterval(() => {
+            setDisplayedText((prevDisplayedText) => {
+                // Determine the next character to add based on the current length of displayedText
+                const nextChar = assistantTextOutput.charAt(prevDisplayedText.length);
+                return prevDisplayedText + nextChar;
+            });
+        }, 50); // The speed of typing, in milliseconds.
+
+        // Clear the interval if the full text has been displayed or on unmount
+        return () => clearInterval(timer);
+    }, [assistantTextOutput]);
 
     return (
         <div className=" bg-white w-full p-4 text-center italic">
             <div className="h-full flex items-center p-10 border-t-2 border-b-2">
-                <p className="w-full">{assistantTextOutput}</p>
+                <p className="w-full">{displayedText}</p>
             </div>
         </div>
     )
--- src/screens/Greet.jsx
+++ src/screens/Greet.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect, useRef } from 'react';
+import { useAssistant } from "../contexts";
 import { AssistantIcon, AssistantTextOutputArea, HoldToSpeakButton } from '../components';
 import { sendSpeechAudio } from '../lib';
 
 export default function Greet({ recording, setRecording, sendSpeechAudio }) {
     const audioRef = useRef(new Audio('audio/greet01.mp3'));
+    const { setAssistantTextOutput } = useAssistant();
 
     useEffect(() => {
         audioRef.current.play()
             .catch(error => console.error("Error playing audio:", error));
+        setAssistantTextOutput("Welcome to your language learning assistant! Would you like to learn some Ukrainian?");
     }, [])
 
     return (
